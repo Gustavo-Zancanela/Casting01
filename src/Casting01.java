62dc35ba95b80e97f736a211bbf02a5d20a1472b
ALUNO <ALUNO@LAB04-PC32>
2024-06-05 20:46:16 -0300

    Commit project Casting01

---
+++ src/Casting01.java
@@ -0,0 +1,27 @@
+public class Casting01 {
+    public static void main(String[] args) {
+        //Declare and initialize a byte with a value of 128
+        //Observe NetBeans' complaint
+        byte valor1 = 127;
+        short valor2 = (byte) 128;
+        byte valor3 = 127;
+        
+        //Declare and initialize a short with a value of 128
+        //Create a print statement that casts this short to a byte
+
+        System.out.println(valor1);
+        System.out.println(valor2);
+        valor3++;
+        System.out.println(valor3);
+        valor3++;
+        System.out.println(valor3);
+        
+        //Declare and initialize a byte with a value of 127
+        //Add 1 to this variable and print it
+        //Add 1 to this variable again and print it again
+
+        
+        
+        
+    }    
+}
